76d311815d9895338c75f77aea3a049180342062
saidurraahmaan <saidurraahmaan@gmail.com>
2023-04-09 12:39:59 +0600

    controller and docker file created

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        private readonly string _config;
+        public CatalogContext(IConfiguration configuration)
+        {
+            _config = configuration.GetValue<string>("DatabaseSettings");
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionSettings"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
